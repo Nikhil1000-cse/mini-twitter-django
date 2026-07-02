ba34ad77f9800aa0e47037d5d0257ef969ae130f
Nikhil1000-cse <nikhiljangid1000@gmail.com>
2026-07-02 13:31:43 +0530

    Add files via upload

---
+++ Mini Tweeter clone/tweeter/tweet/forms.py
@@ -0,0 +1,24 @@
+from django import forms 
+from .models import tweet 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class tweetform(forms.ModelForm):
+    class Meta:
+        model = tweet
+        fields = [ 'text', 'photo']
+
+
+class userRegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ('username','email', 'password1', 'password2')
+        
+def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+
+    for field in self.fields.values():
+        field.widget.attrs.update({
+            'class': 'form-control'
+        })        
